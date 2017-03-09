83f689c0adb2962d7fcf786297683f205113c2e0
dilarionov <d.ilarionov@pflb.ru>
2017-03-09 20:41:56 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='twitterGrabber',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='D. Ilarionov',
+    author_email='d.ilarionov@pflb.ru',
+    description='twitterGrabber'
+)
